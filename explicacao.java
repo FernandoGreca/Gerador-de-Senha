8b52d774f649fa72d25a7337ad805b76bd8944dd
Fernando Greca <144340988+FernandoGreca@users.noreply.github.com>
2024-05-12 11:58:45 -0300

    Update explicacao.java

--- explicacao.java
+++ explicacao.java
@@ -7,13 +7,17 @@ public class explicacao {
         int numeroDigitado;
         char converter;
 
+        // usuario digita um numero qualquer
         System.out.print("Digite um numero: ");
         numeroDigitado = sc.nextInt();
 
+        // programa imprime este numero digitado
         System.out.println("Numero digitado: " + numeroDigitado);
 
+        // ao colocar um numero em uma variavel do tipo caractere realizando o casting ele e automaticamente convertido para um caractere na tabela ASCII
         converter = (char) numeroDigitado;
 
+        // numero depois de convertido e imprimido da tela
         System.out.println("Numero na tebela ASCII: " + converter);
 
     }
